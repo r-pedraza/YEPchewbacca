dd6cadff1b86d2a6098303d7c1cf5c009d22d1e2
victoruriarte <victoruriarte.r@gmail.com>
2015-01-10 10:28:38 +0100

    commit victor

--- app/src/main/java/es/raul/pedraza/yepchewaka/MainActivity2.java
+++ app/src/main/java/es/raul/pedraza/yepchewaka/MainActivity2.java
@@ -1,22 +1,18 @@
 package es.raul.pedraza.yepchewaka;
 
-import java.util.Locale;
-
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import java.util.Locale;
 
 
 public class MainActivity2 extends ActionBarActivity {
