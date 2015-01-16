94954df992b5b34ce64e77003d66bbc92c506bc6
Raul Pedraza <raulpedrazaleon@gmail.com>
2015-01-16 18:53:08 +0100

    Conexion a Parse

---
+++ app/src/main/java/es/raul/pedraza/yepchewaka/YetApplication.java
@@ -0,0 +1,26 @@
+package es.raul.pedraza.yepchewaka;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by cesardominguez on 16/1/15.
+ */
+public class YetApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Enable Local Datastore.
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "wVrm5HZnLB49CAL9NIuYZ25sdBPuJkoQFWuTVup2", "kBx21HZKmUwfYvclgLCOu7JJ2qjhPHp3hFKkieQ1");
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+
+    }
+}
