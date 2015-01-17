36a6af69633b5474e2432f28981714772a6ad1b2
victoruriarte <victoruriarte.r@gmail.com>
2015-01-17 09:45:02 +0100

    problemas

--- app/src/main/java/es/raul/pedraza/yepchewaka/LoginActivity.java
+++ app/src/main/java/es/raul/pedraza/yepchewaka/LoginActivity.java
@@ -12,6 +12,8 @@ public class LoginActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //loginActivity
     }
 
 
