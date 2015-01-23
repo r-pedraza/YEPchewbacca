5ba15e77d64dfd8a9f47be13ef5ba9857bfcf2d6
victoruriarte <victoruriarte.r@gmail.com>
2015-01-23 20:21:08 +0100

    signUp con Carlos

--- app/src/main/java/es/raul/pedraza/yepchewaka/SignUpActivity.java
+++ app/src/main/java/es/raul/pedraza/yepchewaka/SignUpActivity.java
@@ -1,19 +1,71 @@
 package es.raul.pedraza.yepchewaka;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
 public class SignUpActivity extends ActionBarActivity {
+
+    private EditText usernameField;
+    private EditText passwordField;
+    private EditText emailField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        usernameField = (EditText) findViewById(R.id.usernameField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
+        emailField = (EditText) findViewById(R.id.emailField);
+
     }
 
+    public void signUp(View v){
+
+        String sUserName = usernameField.getText().toString().trim(); //trim quita espacios a izquierda y derecha
+        String sPassword = passwordField.getText().toString().trim();
+        String sEmailAddress = emailField.getText().toString().trim();
+
+        if(sUserName.isEmpty() | sPassword.isEmpty() | sEmailAddress.isEmpty()){
+            Toast.makeText(this, "Falta por completar un campo", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            //Crea objeto vacio para un usuario
+            ParseUser newUser = new ParseUser();
+
+            newUser.setUsername(sUserName);
+            newUser.setPassword(sPassword);
+            newUser.setEmail(sEmailAddress);
+
+            newUser.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Intent intent = new Intent(SignUpActivity.this, MainActivityTab.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    } else {
+                        //Creamos un Toast llamando a la clase Superior
+                        Toast.makeText(SignUpActivity.this, "No se ha podido crear el usuario" +
+                                "", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
