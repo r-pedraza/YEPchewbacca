4c7879994b65c01164cf12708a537bba05a630ac
Raúl Pedraza <raulpedrazaleon@gmail.com>
2015-03-12 20:54:52 +0100

    Añadidas notificaciones push

--- app/src/main/java/es/raul/pedraza/yepchewaka/YetApplication.java
+++ app/src/main/java/es/raul/pedraza/yepchewaka/YetApplication.java
@@ -1,9 +1,13 @@
 package es.raul.pedraza.yepchewaka;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 /**
  * Created by raulpedraza on 16/1/15.
@@ -19,6 +23,17 @@ public class YetApplication extends Application {
         ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
         testObject.saveInBackground();
+
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
 
 
     }
